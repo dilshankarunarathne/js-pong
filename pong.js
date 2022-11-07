821cffe75e03fec41cb40a2692324e4fb2264032
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-11-07 15:21:12 +0530

    Update pong.js

--- pong.js
+++ pong.js
@@ -56,6 +56,7 @@ function mouseMoveHandler(e) {
         paddleX = relativeX - paddleWidth/2;
     }
 }
+
 function collisionDetection() {
     for(c=0; c<brickColumnCount; c++) {
         for(r=0; r<brickRowCount; r++) {
